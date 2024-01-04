bdb5934395c1735303f52027b76be33f79d9541b
James Mayclin <maycj@amazon.com>
2024-01-04 19:48:52 +0000

    intermediate

--- tests/integrationv2/conftest.py
+++ tests/integrationv2/conftest.py
@@ -4,16 +4,15 @@ from global_flags import set_flag, S2N_PROVIDER_VERSION, S2N_FIPS_MODE, S2N_NO_P
 
 
 def pytest_addoption(parser):
+    pass
     parser.addoption("--provider-version", action="store", dest="provider-version",
                      default=None, type=str, help="Set the version of the TLS provider", required=True)
-    parser.addoption("-fips-mode", action="store_true", dest="fips-mode",
+    parser.addoption("--fips-mode", action="store_true", dest="fips-mode",
                      default=False, help="S2N is running in FIPS mode")
-    parser.addoption("-no-pq", action="store_true", dest="no-pq",
+    parser.addoption("--no-pq", action="store_true", dest="no-pq",
                      default=False, help="Turn off PQ support")
-    parser.addoption("-force-javassl", action="store_true", dest="force-javassl", default=False, help="fail if the javassl provider is not available")
-    parser.addoption("-force-gnutls", action="store_true", dest="force-gnutls", default=False, help="fail if the gnutls provider is not available")
-    parser.addoption("--s2n-fixture-path", action="store", dest="s2n-fixture-path", default=None, help="manually specify the path to the s2nc and s2nd binaries")
-
+    parser.addoption("--force-javassl", action="store_true", dest="force-javassl", default=False, help="fail if the javassl provider is not available")
+    parser.addoption("--force-gnutls", action="store_true", dest="force-gnutls", default=False, help="fail if the gnutls provider is not available")
 
 def pytest_configure(config):
     """
@@ -24,38 +23,37 @@ def pytest_configure(config):
         "markers", "uncollect_if(*, func): function to unselect tests from parametrization"
     )
 
-    # the s2n fixture path was manually specified
-    if not config.getoption("s2n-fixture-path") is None:
-        path = config.getoption("s2n-fixture-path")
-        if not (bin_available(path + "s2nc") and bin_available(path + "s2nd")):
-            raise Exception("s2n fixture were not found at the supplied path")
-    # try to manually find the s2n* fixtures
+    provider_status = {}
+    # we will error out if s2n and openssl aren't available
+    provider_available["s2n"] = True
+    provider_available["openssl"] = True
+    # check on PATH
+    if bin_available("s2nd") and bin_available("s2nc"):
+        config.stash["s2n-fixture-path"] = ""
+    # try to find s2n-tls/build/bin/s2n*
     else:
-        # check on PATH
-        if bin_available("s2nd") and bin_available("s2nc"):
-            config.option["s2n-fixture-path"] = ""
-        # try to find s2n-tls/build/bin/s2n*
-        else:
-            # iterate back to root directory
-            original_dir = os.getcwd()
+        # iterate back to root directory
+        original_dir = os.getcwd()
+        current_dir = os.getcwd()
+        while (current_dir.split("/")[-1] != "s2n-tls"):
+            os.chdir("..")
             current_dir = os.getcwd()
-            while (current_dir.split("/")[-1] != "s2n-tls"):
-                os.chdir("..")
-                current_dir = os.getcwd()
-
-            s2n_build = current_dir + "/build/bin/"
-            if bin_available(s2n_build + "s2nd") and bin_available(s2n_build + "s2nc"):
-                config.option["s2n-fixture-path"] = s2n_build
-            else:
-                raise Exception("couldn't find s2nd or s2nc")
+        s2n_build = current_dir + "/build/bin/"
+        if bin_available(s2n_build + "s2nd") and bin_available(s2n_build + "s2nc"):
+            config.stash["s2n-fixture-path"] = s2n_build
+        else:
+            os.chdir(original_dir)
+            raise Exception("couldn't find s2nd or s2nc")
+        os.chdir(original_dir)
 
     if not bin_available("openssl"):
         raise Exception("OpenSSL provider was not found")
 
+    provider_available["gnutls-cli"]
     if config.getoption("force-gnutls") and not bin_available("gnutls-cli"):
         raise Exception("GnuTLS was required with --force-gnutls, but was not found")
 
-    raise Exception(config.option)
+    raise Exception(config.stash["s2n-fixture-path"])
 
 
     no_pq = config.getoption('no-pq', 0)
