6bb6e2e9a869dc599ab72b228f87e00705fd072e
James Mayclin <maycj@amazon.com>
2024-03-11 23:41:55 +0000

    add additional test cases
    Also this commit is a repro of duchess failing to return exception
    messages when things are chained together.

--- bindings/rust/bench/src/java.rs
+++ bindings/rust/bench/src/java.rs
@@ -1,55 +1,113 @@
 
-mod utility_jni {
-    duchess::java_package! {
-        package java.security;
-    
-        class SecureRandom {
-            public java.security.SecureRandom();
-        }
+
+duchess::java_package! {
+    package javax.net.ssl;
+
+    class SSLContext {
+        public static javax.net.ssl.SSLContext getInstance(java.lang.String);
+        public final void init(javax.net.ssl.KeyManager[], javax.net.ssl.TrustManager[], java.security.SecureRandom);
     }
-}
 
-mod ssl_jni {
-    use super::utility_jni::java::security;
-
-    duchess::java_package! {
-        package javax.net.ssl;
-    
-        class SSLContext {
-            public static javax.net.ssl.SSLContext getInstance(java.lang.String);
-            public final void init(javax.net.ssl.KeyManager[], javax.net.ssl.TrustManager[], java.security.SecureRandom);
-        }
-    
-        class TrustManagerFactory {
-            public static final java.lang.String getDefaultAlgorithm();
-            public static final javax.net.ssl.TrustManagerFactory getInstance(java.lang.String);
-            public final javax.net.ssl.TrustManager[] getTrustManagers();
-        }
-    
-        class TrustManager { }
-    
-        class KeyManager { }
+    class TrustManagerFactory {
+        public static final java.lang.String getDefaultAlgorithm();
+        public static final javax.net.ssl.TrustManagerFactory getInstance(java.lang.String);
+        public final javax.net.ssl.TrustManager[] getTrustManagers();
     }
-}
 
+    class TrustManager { }
+
+    class KeyManager { }
+
+    package javax.security.cert;
+
+    class X509Certificate { }
+}
 
 #[cfg(test)]
 mod test {
-    use duchess::{java, JvmOp, ToJava};
+    use duchess::{
+        java::{self, io::FileInputStream, security::{cert::CertificateFactory, KeyStore, SecureRandom}}, IntoRust, Jvm, JvmOp, Local, NullJRef, ToJava
+    };
+    use crate::get_cert_path;
+
     // this feels forbidden
-    use super::{ssl_jni::javax::net::ssl::SSLContext, utility_jni::java::security::SecureRandom};
+    use super::javax::net::ssl::SSLContext;
 
     #[test]
     fn create_ssl_context() {
         let tls_string = "TLSv1.3".to_owned();
-        let ssl_context = SSLContext::get_instance(tls_string.to_java::<java::lang::String>()).global().execute().unwrap().unwrap();
+        let ssl_context = SSLContext::get_instance(tls_string.to_java::<java::lang::String>())
+            .global()
+            .execute()
+            // what are the possible errors here? Presumable the exceptions?
+            .unwrap()
+            // why is this an option? That doesn't make sense to me. If it's
+            // successful then should this always be populated?
+            .unwrap();
     }
 
     #[test]
     fn create_secure_random() {
-        let secure_random = SecureRandom::new().global().execute().unwrap();
+        const TEST_ARRAY_LENGTH: usize = 256;
+        //let secure_random = SecureRandom::new().global().execute().unwrap();
+        let initial_array: Vec<i8> = vec![0; TEST_ARRAY_LENGTH];
+        let random_array = Jvm::with(|jvm| {
+            // create the java SecureRandom instance
+            let secure_random = SecureRandom::new().execute_with(jvm).unwrap();
+            // java copy of the initial_array
+            let java_array = initial_array.to_java().execute_with(jvm)?.unwrap();
+            secure_random.next_bytes(&java_array).execute_with(jvm).unwrap();
+            java_array.into_rust(jvm)
+        })
+        .unwrap();
+        // the initial array did not have it's memory modified
+        assert_eq!(initial_array, vec![0; TEST_ARRAY_LENGTH]);
+        // we have a copy of the random java array
+        assert_ne!(random_array, vec![0; TEST_ARRAY_LENGTH]);
+    }
+
+    #[test]
+    fn create_cert_factory() {
+        let certificate = Jvm::with(|jvm| {
+            let path = get_cert_path(crate::PemType::CACert, crate::SigType::Rsa2048);
+            let filestream = FileInputStream::new(&path);
+            let factory = CertificateFactory::get_instance("X.509");
+            let certificate = factory.generate_certificate(filestream);
+            certificate.global().execute_with(jvm)
+        })
+        .unwrap()
+        .unwrap();
+    }
+
+    #[test]
+    fn get_keystore_instance() {
+        let key_store = Jvm::with(|jvm| {
+            KeyStore::get_instance("X.509").global().execute_with(jvm)
+        })
+        .unwrap();
+    }
+
+    #[test]
+    fn create_key_store() {
+        let key_store = Jvm::with(|jvm| {
+            let path = get_cert_path(crate::PemType::CACert, crate::SigType::Rsa2048);
+            let filestream = FileInputStream::new(&path);
+            let factory = CertificateFactory::get_instance("X.509");
+
+            let certificate = factory.generate_certificate(filestream).execute_with(jvm).unwrap().unwrap();
+            let keystore = KeyStore::get_instance("X.509").global().execute_with(jvm);
+            println!("debug keystore: {:?}", keystore.as_ref().err());
+            let null_input_stream: Option<Local<FileInputStream>> = None;
+            let null_char_array: Option<Local<java::Array<u16>>> = None;
+            //keystore.load(&null_input_stream, &null_char_array);
+            //keystore.set_certificate_entry("ca-certificate", certificate);
+            //keystore.set_certificate("ca-certificate", certificate);
+            Ok(keystore.unwrap())
+        })
+        .unwrap();
     }
 }
+
 
 // CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
 
